a4804f7a93253e6ab1fdfccf92c20ad42468ff4b
Carla Uyemura <carla.uyemura@gmail.com>
2022-10-05 16:32:28 -0300

    Requisito 5

--- src/authentication.js
+++ src/authentication.js
@@ -4,18 +4,81 @@ const token = () => crypto.randomBytes(8).toString('hex');
 
 const validateLogin = (email, pass) => {
   const verify = /\S+@\S+\.\S+/;
-  if (!email) return { message: 'O campo "email" é obrigatório' };
+    if (!email) return { message: 'O campo "email" é obrigatório' };
 
-  if (!verify.test(email)) return { message: 'O "email" deve ter o formato "email@email.com"' };
+    if (!verify.test(email)) return { message: 'O "email" deve ter o formato "email@email.com"' };
 
-  if (!pass) return { message: 'O campo "password" é obrigatório' };
+    if (!pass) return { message: 'O campo "password" é obrigatório' };
 
-   if (pass.length < 6) return { message: 'O "password" deve ter pelo menos 6 caracteres' };
-   
+    if (pass.length < 6) return { message: 'O "password" deve ter pelo menos 6 caracteres' };
+    
    return true;
+};
+
+const validateToken = (req, res, next) => {
+  const randomToken = req.headers.authorization;
+  if (!randomToken) {
+    return res.status(401).json({ message: 'Token não encontrado' }); 
+}
+  if (randomToken.length !== 16) { 
+    return res.status(401).json({ message: 'Token inválido' }); 
+  }
+  next();
+};
+
+const validateName = (req, res, next) => {
+  const { name, age } = req.body;
+  if (!name) {
+    return res.status(400).json({ message: 'O campo "name" é obrigatório' }); 
+  }
+  
+  if (name.length < 3) {
+    return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' }); 
+}
+  if (!age) {
+    return res.status(400).json({ message: 'O campo "age" é obrigatório' }); 
+  }
+
+  if (age < 18) {
+    return res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' }); 
+  }
+  next();
+};
+
+const validateDate = (req, res, next) => {
+  const { talk } = req.body;
+  const verifyDate = /^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/;
+  if (!talk) {
+    return res.status(400).json({ message: 'O campo "talk" é obrigatório' }); 
+  }
+  const { watchedAt } = req.body.talk;
+  if (!watchedAt) {
+    return res.status(400).json({ message: 'O campo "watchedAt" é obrigatório' }); 
+  }
+  if (!verifyDate.test(watchedAt)) {
+    return res.status(400).json({ 
+      message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"',
+     }); 
+  }
+  next();
+};
+
+const validateRate = (req, res, next) => {
+  const { rate } = req.body.talk;
+  if (!rate) {
+    return res.status(400).json({ message: 'O campo "rate" é obrigatório' }); 
+  }
+  if (rate < 1 || rate > 5) {
+    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+  next();
 };
 
 module.exports = { 
   token, 
   validateLogin, 
+  validateToken,
+  validateName,
+  validateDate,
+  validateRate,
 };
--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs').promises;
 const path = require('path');
-const { token, validateLogin } = require('./authentication');
+const {
+  token, 
+  validateLogin, 
+  validateToken,
+  validateName,
+  validateDate,
+  validateRate,
+ } = require('./authentication');
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+const pathTalker = path.resolve(__dirname, 'talker.json');
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -16,7 +24,6 @@ app.get('/', (_request, response) => {
 });
 
 const getTalkers = async () => {
-  const pathTalker = path.resolve(__dirname, 'talker.json');
   const talkers = JSON.parse(await fs.readFile(pathTalker, 'utf-8'));
   return talkers;
 };
@@ -43,9 +50,7 @@ app.get('/talker/:id', async (request, response) => {
 const verifyLogin = (req, res, next) => {
   const { email, password } = req.body;
   const validate = validateLogin(email, password);
-  if (validate === true) {
-    return next();
-  }
+  if (validate === true) next();
   res.status(400).json(validate);
 };
 
@@ -53,6 +58,20 @@ app.post('/login', verifyLogin, (_request, response) => {
   const getToken = token();
   response.status(HTTP_OK_STATUS).json({ token: getToken });
 });
+
+app.post('/talker', 
+  validateToken,
+  validateName,
+  validateDate,
+  validateRate,
+  async (request, response) => {
+    const talkers = await getTalkers();
+    const talker = { ...request.body, id: talkers.length + 1 };
+  talkers.push(talker);
+  await fs.writeFile(pathTalker, JSON.stringify(talkers));
+  response.status(201).json(talker);                                                       
+});
+
 app.listen(PORT, () => {
   console.log('Online');
 });
